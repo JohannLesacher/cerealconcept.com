e2e2d27fb74008427f6042a9a502caf02e11c737
Johann Lesacher <j.lesacher@cerealconcept.com>
2019-02-03 18:31:13 +0100

    Contact dans footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,19 +2,21 @@ import React from 'react'
 import Contact from './contact'
 
 const Footer = () => (
-  <Contact />
-  <footer className="section section-footer has-background-dark has-text-white site-footer">
-    <div className="container">
-      <div className="columns">
-        <div className="column">
-          <h4 className="title has-text-white is-4">Cereal Concept</h4>
+  <>
+    <Contact />
+    <footer className="section section-footer has-background-dark has-text-white site-footer">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <h4 className="title has-text-white is-4">Cereal Concept</h4>
+          </div>
+          <div className="column">Adresse</div>
+          <div className="column"></div>
+          <div className="column">Reseaux</div>
         </div>
-        <div className="column">Adresse</div>
-        <div className="column"></div>
-        <div className="column">Reseaux</div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  </>
 )
 
 export default Footer
