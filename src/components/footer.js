b99ee5f5b3b925a374bf0eab8c1244d968834c73
Adrien Pierrin <a.pierrin@cerealconcept.com>
2019-02-04 19:14:41 +0100

    MAJ footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,14 +6,11 @@ const Footer = () => (
     <Contact />
     <footer className="section section-footer has-background-dark has-text-white site-footer">
       <div className="container">
-        <div className="columns">
-          <div className="column">
-            <h4 className="title has-text-white is-4">Cereal Concept</h4>
-          </div>
-          <div className="column"><p>Johann Lesacher, web alchimiste<br/>j.lesacher@cerealconcept.com<br/>+33 671 541 553</p></div>
-          <div className="column"><p>Adrien Pierrin, web et communication<br/>a.pierrin@cerealconcept.com<br/>+33 658 477 849</p></div>
-          <div className="column"><ul><li>Mentions légales</li><li>Politique de confidentialité</li></ul></div>
+        <div className="column"><h4>Nous contacter</h4>
+          <div className="contact"><p>Johann Lesacher, web alchimiste<br/><a href="mailto:j.lesacher@cerealconcept.com">j.lesacher@cerealconcept.com</a> - +33 6 71 541 553</p></div>
+          <div className="contact"><p>Adrien Pierrin, webdesign et e-marketing<br/><a href="mailto:a.pierrin@cerealconcept.com">a.pierrin@cerealconcept.com</a> - +33 6 58 477 849</p></div>
         </div>
+        <div className="column"><ul><li>Mentions légales</li><li>Politique de confidentialité</li></ul></div>
       </div>
     </footer>
   </>
