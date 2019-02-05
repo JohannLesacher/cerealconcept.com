92d56372b5a21de35e61337fc6e221115c59fc28
Johann Lesacher <j.lesacher@cerealconcept.com>
2019-02-05 08:47:09 +0100

    Essai footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Contact from './contact'
+import { Link } from 'gatsby'
 
 const Footer = () => (
   <>
@@ -10,9 +11,23 @@ const Footer = () => (
           <div className="column">
             <h4 className="title has-text-white is-4">Cereal Concept</h4>
           </div>
-          <div className="column"><p>Johann Lesacher, web alchimiste<br/>j.lesacher@cerealconcept.com<br/>+33 671 541 553</p></div>
-          <div className="column"><p>Adrien Pierrin, web et communication<br/>a.pierrin@cerealconcept.com<br/>+33 658 477 849</p></div>
-          <div className="column"><ul><li>Mentions légales</li><li>Politique de confidentialité</li></ul></div>
+          <div className="column">
+            <h3 className="title is-3">Johann Lesacher</h3>
+            <h4 className="subtitle is-5">Développeur Web Full Stack</h4>
+            <a href="mailto:j.lesacher@cerealconcept.com">j.lesacher@cerealconcept.com</a><br/>
+            <a href="tel:+33671541553">+33 671 541 553</a>
+          </div>
+          <div className="column">
+            <p>Adrien Pierrin, Web et Communication<br/>
+            <a href="mailto:a.pierrin@cerealconcept.com">a.pierrin@cerealconcept.com</a><br/>
+            <a href="tel:+33658477849">+33 658 477 849</a></p>
+          </div>
+          <div className="column">
+            <ul>
+              <li><Link to="/">Mentions légales</Link></li>
+              <li><Link to="/politique-de-confidentialite">Politique de confidentialité</Link></li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
